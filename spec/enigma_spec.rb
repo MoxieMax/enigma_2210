83127b99fd4f04d908185e4efcae54b09c367cf6
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-11 15:56:44 -0700

    Created encrypt and decrypt method with instructions from turing site

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,1 +1,8 @@
 require 'spec_helper'
+
+RSpec.describe Enigma do
+  describe '#encrypt' do
+    it 'will encrypt a message using a key' do
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ require 'simplecov'
 SimpleCov.start
 
 require 'pry'
+require 'date'
 require './lib/enigma'
