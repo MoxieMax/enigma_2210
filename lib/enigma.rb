99d2579dae7677e2128a89994220804c157eeaae
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-11 22:55:38 -0700

    Started working on the file i/o

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,9 +3,10 @@ class Enigma
   def encrypt(message, key = nil, date = nil)
     hash = {
             encryption: , #encrypted message
-            key: , #random 5 numbers
-            date: #DDMMYY
+            key: key, #random 5 numbers
+            date: date #DDMMYY
             }
+            #("a".."z").to_a << " " #alphabet plus space
     #returns a hash with 3 keys
   # The encrypt method takes a message String as an argument. It can optionally take a Key and Date as arguments to use for encryption. If the key is not included, generate a random key. If the date is not included, use today’s date.
 # :encryption => the encrypted String
@@ -13,11 +14,10 @@ class Enigma
 # :date => the date used for encryption as a String in the form DDMMYY
   end
   
-  def decrypt(message key = nil, date = nil)
+  def decrypt(message, key = nil, date = nil)
   #The decrypt method takes a ciphertext String and the Key used for encryption as arguments. The decrypt method can optionally take a date as the third argument. If no date is given, this method should use today’s date for decryption.
 # :decryption => the decrypted String
 # :key => the key used for decryption as a String
 # :date => the date used for decryption as a String in the form DDMMYY
   end
-  
 end
