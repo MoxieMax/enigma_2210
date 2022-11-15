1cfe3f63731fd59d64f4dcb8353a83cbfac14321
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-15 09:25:24 -0700

    Moved tests and code for decryptable

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -80,22 +80,22 @@ RSpec.describe Enigma do
     end
   end
   
-  describe '#decipher' do
-    it 'decodes a string based on the given key' do
-      expect(enigma.decipher('k', 3)).to eq('h')
-      expect(enigma.decipher('e', 27)).to eq('e')
-      expect(enigma.decipher('d', 73)).to eq('l')
-      expect(enigma.decipher('e', 20)).to eq('l')
-      expect(enigma.decipher('r', 3)).to eq('o')
-      expect(enigma.decipher('khoor', 3)).to eq('hello')
-    end
-  end
-  
-  describe '#decode' do
-    it 'decodes each letter in a string using #decipher' do
-      expect(enigma.decode('keder ohulw')).to eq('hello world')
-    end
-  end
+  # describe '#decipher' do
+  #   it 'decodes a string based on the given key' do
+  #     expect(enigma.decipher('k', 3)).to eq('h')
+  #     expect(enigma.decipher('e', 27)).to eq('e')
+  #     expect(enigma.decipher('d', 73)).to eq('l')
+  #     expect(enigma.decipher('e', 20)).to eq('l')
+  #     expect(enigma.decipher('r', 3)).to eq('o')
+  #     expect(enigma.decipher('khoor', 3)).to eq('hello')
+  #   end
+  # end
+  # 
+  # describe '#decode' do
+  #   it 'decodes each letter in a string using #decipher' do
+  #     expect(enigma.decode('keder ohulw')).to eq('hello world')
+  #   end
+  # end
 
   describe '#decrypt' do
     it 'will generate a hash and translate a string' do
